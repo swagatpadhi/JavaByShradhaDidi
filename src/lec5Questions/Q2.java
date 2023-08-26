c5b1f20906afa9a830bc2993d968481c441851f3
Swagat <swagatpadhi03@gmail.com>
2023-08-26 23:27:49 +0530

    modified Q2 and then added Q3 for hollow rectangle pattern

--- src/lec5Questions/Q2.java
+++ src/lec5Questions/Q2.java
@@ -4,6 +4,7 @@ package lec5Questions;
 //        *  *
 //        *  *
 //        ****
+// Hollow rectangle
 public class Q2 {
     public static void main(String[] args){
         for(int i = 1; i <= 5; i++){
---
+++ src/lec5Questions/Q3.java
@@ -0,0 +1,16 @@
+package lec5Questions;
+//Half Pyramid
+//*
+//**
+//***
+//****
+public class Q3 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
