c1dbfd63a9aef6fb0b39d1912673574f63465d1c
Swagat <swagatpadhi03@gmail.com>
2023-08-26 23:16:07 +0530

    added new code for hollow rectangle pattern

---
+++ src/lec5Questions/Q2.java
@@ -0,0 +1,20 @@
+package lec5Questions;
+//        ****
+//        *  *
+//        *  *
+//        *  *
+//        ****
+public class Q2 {
+    public static void main(String[] args){
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= 4; j++){
+                if(i == 1 || i == 5 || j == 1 || j == 4){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
