918950a4530bc53ce220826337f4e57850de2fbd
Swagat <swagatpadhi03@gmail.com>
2023-08-26 23:34:18 +0530

    added Q4 inverted half pyramid

---
+++ src/lec5Questions/Q4.java
@@ -0,0 +1,16 @@
+package lec5Questions;
+//Inverted half pyramid
+//****
+//***
+//**
+//*
+public class Q4 {
+    public static void main(String[] args) {
+        for(int i = 4; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
