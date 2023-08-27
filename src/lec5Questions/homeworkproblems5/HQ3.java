9247ced7fdd6d592454583b2cc0a0083b137ea89
Swagat <swagatpadhi03@gmail.com>
2023-08-27 12:28:33 +0530

    added HQ3 to homework question package

---
+++ src/lec5Questions/homeworkproblems5/HQ3.java
@@ -0,0 +1,23 @@
+package lec5Questions.homeworkproblems5;
+//palindromic number pyramid
+//    1
+//   212
+//  32123
+// 4321234
+//543212345
+public class HQ3 {
+    public static void main(String[] args){
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= 5-i; j++){
+                System.out.print(" ");
+            }
+            for(int j = i; j >= 1; j--){
+                System.out.print(j);
+            }
+            for(int j = 2; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
