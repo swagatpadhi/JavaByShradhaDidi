d6b94adf8a110273f3e1f62fa1e067003a25bb8b
Swagat <swagatpadhi03@gmail.com>
2023-08-27 09:45:37 +0530

    added Q6 to lec5Questions

---
+++ src/lec5Questions/Q6.java
@@ -0,0 +1,16 @@
+package lec5Questions;
+//half pyramid with numbers
+//1
+//12
+//123
+//1234
+public class Q6 {
+    public static void main(String[] args){
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
