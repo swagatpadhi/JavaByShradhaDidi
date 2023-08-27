dc67e6bc3c28a3fa8ecbef1eb0d738f373f55f91
Swagat <swagatpadhi03@gmail.com>
2023-08-27 11:21:10 +0530

    created a package homeworkproblem and added solid rhombus pattern's code in it.

---
+++ src/lec5Questions/homeworkproblems5/HQ1.java
@@ -0,0 +1,22 @@
+package lec5Questions.homeworkproblems5;
+//   *****
+//  *****
+// *****
+//*****
+//solid rhombus
+public class HQ1 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= 4-i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for(int j = 4; j >= i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
