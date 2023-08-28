0522cceceb794ddcd161ff6ddb6d4c4b173e63c2
Swagat <swagatpadhi03@gmail.com>
2023-08-28 13:13:07 +0530

    Added Q6 solution

---
+++ src/java_exercise1_solutions/Q6.java
@@ -0,0 +1,9 @@
+package java_exercise1_solutions;
+//Write an infinite loop using do while condition.
+public class Q6 {
+    public static void main(String[] args){
+        do{
+            System.out.println(1);
+        }while(true);
+    }
+}
