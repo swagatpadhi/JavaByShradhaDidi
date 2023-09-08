c99555f0363c8ef51b2c01448b58d5e466ea91ef
Swagat <swagatpadhi03@gmail.com>
2023-09-08 16:51:56 +0530

    added find factorial of n via recursion

--- src/Lec17_recursion_lvl_easy/FactorialOfN.java
+++ src/Lec17_recursion_lvl_easy/FactorialOfN.java
@@ -1,4 +1,16 @@
 package Lec17_recursion_lvl_easy;
 
 public class FactorialOfN {
+    public static void main(String[] args) {
+        System.out.println(calcFact(5));
+    }
+
+    private static int calcFact(int i) {
+        if (i == 0 || i == 1) {
+            return 1;
+        }
+        int fact_im1 = calcFact(i - 1);
+        int fact_i = i * fact_im1;
+        return fact_i;
+    }
 }
