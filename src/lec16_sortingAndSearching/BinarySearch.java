19150b7550c66e1732fe12697f2754ce3c92bce4
Swagat <swagatpadhi03@gmail.com>
2023-09-14 18:40:55 +0530

    optimised binary Search problem for bigger int values

--- src/lec16_sortingAndSearching/BinarySearch.java
+++ src/lec16_sortingAndSearching/BinarySearch.java
@@ -5,14 +5,14 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {9,8,4,3,2};
         Arrays.sort(arr);
-        int key = 77;
+        int key = 8;
         System.out.println(binarySearch2(arr, key));
     }
     public static boolean binarySearch2(int[] arr, int key) {
         int si = 0;
         int ei = arr.length - 1;
         while ( si <= ei) {
-            int mi = (si+ei)/2;
+            int mi = si + (ei/2 - si/2);
             if ( arr[mi] == key) {
                 return true;
             }
