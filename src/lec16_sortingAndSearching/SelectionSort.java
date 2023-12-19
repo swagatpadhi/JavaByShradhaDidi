29c39ae91ca1b54a71faa8e10a5c920ecb7fc275
Swagat <swagatpadhi03@gmail.com>
2023-12-19 22:07:22 +0530

    modified the code of Selection Sort.

--- src/lec16_sortingAndSearching/SelectionSort.java
+++ src/lec16_sortingAndSearching/SelectionSort.java
@@ -4,24 +4,24 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] x = {7,8,3,1,2};
-
-        //selection sort
-        //time complexity - O(n^2)
-        for (int i = 0; i < x.length - 1; i++) {
-            int smallestInd = i;
-            for (int j = i + 1; j < x.length; j++) {
-                if (x[smallestInd] > x[j]) {
-                    smallestInd = j;
+        int[] arr = {13, 46, 24, 52, 20, 6};
+        for (int i = 0; i <= arr.length - 2; i++) {
+            int minInd = i;//let element present in ith index is the minimum
+            //element.
+            for (int j = i; j <= arr.length - 1; j++) {
+                //internal loop finds
+                //minimum element's index in array.
+                if (arr[j] < arr[minInd]) {
+                    minInd = j;
                 }
             }
-            //swap
-            int temp = 0;
-            temp = x[smallestInd];
-            x[smallestInd] = x[i];
-            x[i] = temp;
+
+            //swap minInd element with current i position
+            //of array.
+            int temp = arr[minInd];
+            arr[minInd] = arr[i];
+            arr[i] = temp;
         }
-        //after sorting
-        System.out.println(Arrays.toString(x));
+        System.out.println(Arrays.toString(arr));
     }
 }
