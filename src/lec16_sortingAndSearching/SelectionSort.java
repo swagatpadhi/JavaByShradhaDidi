5d23b9b142788480fcfba5aec17f4c44969e228d
swagatpadhi <swagatpadhi03@gmail.com>
2024-02-19 19:04:38 +0530

    added few comments in implementation of Selection sort.

--- src/lec16_sortingAndSearching/SelectionSort.java
+++ src/lec16_sortingAndSearching/SelectionSort.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {13, 46, 24, 52, 20, 6};
+        //outer-loops are for iterations and inner-loops are for
+        //doing some work.
+        //outer-loop runs for 5 time in this case.
+        // i --> 0 to 4 (tab hi toh 5 times)
+        // 4 means arr.length - 2
         for (int i = 0; i <= arr.length - 2; i++) {
             int minInd = i;//let element present in ith index is the minimum
             //element.
